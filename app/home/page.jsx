e286cb599889b192f9fb0ca043a3eeb796d8eb9d
Anthony <crayonno.o@gmail.com>
2023-03-08 00:10:54 +0100

    fm

---
+++ app/home/page.jsx
@@ -0,0 +1,102 @@
+"use client";
+
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "./page.module.css";
+import { Poppins } from "@next/font/google";
+import { useRouter } from "next/navigation";
+import { useState, useEffect, useRef } from "react";
+
+const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  const router = useRouter();
+
+  //State
+  const [hour, setHour] = useState(0);
+  const [minute, setMinute] = useState(0);
+  const [second, setSecond] = useState(0);
+  const [secondR, setSecondR] = useState(false);
+
+  var interval = useRef(null);
+
+  //Controllers
+  const start = () => {
+    if (secondR) {
+    } else {
+      setSecondR(true);
+      interval.current = setInterval(() => {
+        setSecond((second) => second + 1);
+      }, 1000);
+    }
+  };
+  const pause = () => {
+    if (secondR) {
+      setSecondR(false);
+      clearInterval(interval.current);
+    } else {
+    }
+  };
+  const reset = () => {
+    setSecondR(false);
+    clearInterval(interval.current);
+    setSecond(0);
+    setMinute(0);
+    setHour(0);
+  };
+
+  //updates minute when second timer gets to 60
+  useEffect(() => {
+    if (second == 60) {
+      setMinute((minute) => minute + 1);
+      setSecond(0);
+    } else {
+    }
+  }, [second]);
+
+  //updates hour when minute timer gets to 60
+  useEffect(() => {
+    if (minute == 60) {
+      setHour((hour) => hour + 1);
+      setMinute(0);
+    } else {
+    }
+  }, [minute]);
+
+  //resets timer when hour gets to 24
+  useEffect(() => {
+    if (hour == 24) {
+      setHour(0);
+      setMinute(0);
+      setSecond(0);
+    } else {
+    }
+  }, [minute]);
+
+  return (
+    <main className={styles.main}>
+      <img className={styles.watch} src="/watch.svg" alt="" />
+      <div className={styles.counter}>
+        <h1 className={poppins.className}>
+          {hour}:{minute}:{second}
+        </h1>
+      </div>
+
+      <div className={styles.buttons}>
+        <button onClick={start}>
+          <h3 className={poppins.className}>Start</h3>
+        </button>
+        <button onClick={pause}>
+          <h3 className={poppins.className}>Pause</h3>
+        </button>
+        <button onClick={reset}>
+          <h3 className={poppins.className}>Reset</h3>
+        </button>
+      </div>
+    </main>
+  );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,44 +1,45 @@
-import Image from "next/image";
-import { Inter } from "@next/font/google";
+"use client";
+
 import styles from "./page.module.css";
+import { useState, useEffect, useRef, useLayoutEffect } from "react";
+import gsap from "gsap";
+import { useRouter } from "next/navigation";
 import { Poppins } from "@next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
 
 export default function Home() {
-  const actions = [
-    { name: "length", img: "/rulers.svg" },
-    { name: "area", img: "/area.svg" },
-    { name: "mass", img: "/box.svg" },
-    { name: "volume", img: "/volume.svg" },
-    { name: "time", img: "/time.svg" },
-    { name: "speed", img: "/speed.svg" },
-    { name: "weight", img: "/scale.svg" },
-    { name: "temperature", img: "/thermometer.svg" },
-    { name: "angle", img: "/angle.svg" },
-  ];
+  const router = useRouter();
+  const ref = useRef(null);
+  const g = gsap.utils.selector(ref);
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.to(g("#main *"), { visibility: "visible", duration: 1 });
+      gsap.from(g("#clock"), { rotate: 30, fill: "black", duration: 1 });
+      gsap.from(
+        g("#main h1"),
+        {
+          x: -50,
+          opacity: 0,
+          onComplete: () => {
+            router.push("/home");
+          },
+        },
+        0.4
+      );
+    });
+    return () => ctx.revert();
+  }, []);
   return (
-    <main className={styles.main}>
-      <header>
-        <h1 className={poppins.className}>Unit Converter</h1>
-      </header>
-
-      <div className={styles.search}>
-        <input type="text" />
-      </div>
-
-      <div className={styles.container}>
-        {actions.map((card, index) => (
-          <div key={index} className={styles.card}>
-            <img src={card.img} alt="" />
-            <h3 className={poppins.className}>{card.name}</h3>
-          </div>
-        ))}
-      </div>
+    <main ref={ref} id="main" className={styles.main}>
+      <svg id="clock" width="150" height="150" fill="white" viewBox="0 0 16 16">
+        <path d="M6.5 0a.5.5 0 0 0 0 1H7v1.07A7.001 7.001 0 0 0 8 16a7 7 0 0 0 5.29-11.584.531.531 0 0 0 .013-.012l.354-.354.353.354a.5.5 0 1 0 .707-.707l-1.414-1.415a.5.5 0 1 0-.707.707l.354.354-.354.354a.717.717 0 0 0-.012.012A6.973 6.973 0 0 0 9 2.071V1h.5a.5.5 0 0 0 0-1h-3zm2 5.6V9a.5.5 0 0 1-.5.5H4.5a.5.5 0 0 1 0-1h3V5.6a.5.5 0 1 1 1 0z" />
+      </svg>
+      <h1 className={poppins.className}>Stopwatch</h1>
     </main>
   );
 }
